96dde97239a46c039ef28dcdd672b48bcea9456f
siddhucreation <siddhpatela@gmail.com>
2026-09-18 21:24:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const GITHUB_PATH="documents";
 const BRANCH="main";
 
 const ACCOUNTS={
-  user:{password:"bca123",role:"bca123"},
+  user:{password:"bca",role:"bca"},
   admin:{password:"admin123",role:"admin"}
 };
 
